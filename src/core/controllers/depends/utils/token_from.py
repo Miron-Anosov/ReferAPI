1662c98272467b718cf42ad1fe1c5bb93cdf5016
Miron-Anosov <mairon26rus@gmail.com>
2024-10-30 23:54:34 +0100

    refactor: add return type -> str | None

--- src/core/controllers/depends/utils/token_from.py
+++ src/core/controllers/depends/utils/token_from.py
@@ -6,12 +6,16 @@ from src.core.controllers.depends.utils.jwt_token import decode_jwt
 from src.core.settings.constants import JWT, Keys
 
 
-def get_user_id_from_token(request: Request):
+def get_user_id_from_token(request: Request) -> str | None:
     """Get user id from token."""
     token = request.headers.get(Keys.AUTH_HEADER)[
         Keys.AUTH_HEADER_PREF_BEARER :  # noqa E203
     ]
+    if not token:
+        return None
     data_token = decode_jwt(jwt_token=token)
     type_token = data_token.pop(JWT.TOKEN_TYPE_FIELD)
     if type_token == JWT.TOKEN_TYPE_ACCESS:
         return data_token.get(JWT.PAYLOAD_SUB_KEY)
+
+    return None
